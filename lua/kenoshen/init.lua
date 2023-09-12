ffbabe1765f0c023f80a8397bf51480e2e0a1500
Michael Wingfield <mwingfie@gmail.com>
2023-09-12 20:12:05 +0000

    got lazy set up

--- lua/kenoshen/init.lua
+++ lua/kenoshen/init.lua
@@ -1,4 +1,5 @@
--- load the lazy.nvim package manager from https://github.com/folke/lazy.nvim
+require("kenoshen.remap")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -12,4 +13,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup({
+
+})
