d83f54fef6cf6f502d6b0d0db2aa0acb3643f5c4
Michael Wingfield <mwingfie@gmail.com>
2024-02-16 10:29:25 -0700

    enable relative line numbers

--- lua/kenoshen/init.lua
+++ lua/kenoshen/init.lua
@@ -1,5 +1,8 @@
 -- enable 24-bit colors
 vim.g.termguicolors=true
+-- enable relative line numbers
+vim.cmd("set relativenumber")
+vim.cmd("set number")
 
 require("kenoshen.mappings")
 require("kenoshen.lazy")
