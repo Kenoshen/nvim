e7cbb117aede1bbd5b4e4430366c53e6ed799150
Kenoshen <mwingfie@gmail.com>
2024-03-15 13:46:45 -0600

    added auto save and format on save

---
+++ lua/kenoshen/plugins/format-on-save.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "elentok/format-on-save.nvim",
+    config = function()
+      local formatters = require("format-on-save.formatters")
+      require("format-on-save").setup({
+        exclude_path_patterns = {
+          "/node_modules/",
+          ".local/share/nvim/lazy",
+        },
+        formatter_by_ft = {
+          go = {
+            formatters.shell({
+              cmd = { "goimports-reviser", "-rm-unused", "-set-alias", "-format", "%" },
+              tempfile = function()
+                return vim.fn.expand("%") .. '.formatter-temp'
+              end
+            }),
+            formatters.shell({
+              cmd = { "gofmt" },
+            }),
+          },
+        },
+        fallback_formatter = {
+          formatters.remove_trailing_whitespace,
+          formatters.remove_trailing_newlines,
+          formatters.prettierd,
+        },
+      })
+    end,
+  },
+}
