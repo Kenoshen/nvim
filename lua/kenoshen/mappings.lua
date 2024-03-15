bccd0639b4bc86fdf390e7bc25ea058593dcdb48
Kenoshen <mwingfie@gmail.com>
2024-03-15 14:25:22 -0600

    add mapings for diagnostic functions

--- lua/kenoshen/mappings.lua
+++ lua/kenoshen/mappings.lua
@@ -42,6 +42,9 @@ autocmd("LspAttach", {
   callback = function(e)
     local opts = { buffer = e.buf }
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    vim.keymap.set("n", "gn", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+    vim.keymap.set("n", "gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
