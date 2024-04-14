c9b0bdfa7ba1b94ded1fc2dd179bbe586d1f40ae
Kenoshen <mwingfie@gmail.com>
2024-04-14 14:33:48 -0600

    add resume

--- lua/kenoshen/plugins/telescope.lua
+++ lua/kenoshen/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
       vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
       vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
       vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
+      vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>")
     end,
   },
 }
